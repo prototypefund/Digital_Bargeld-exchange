0121b8e44cd1d2c980982c22d921321c9b672a9e
Christian Grothoff <christian@grothoff.org>
2015-07-15 11:40:51 +0200

    -fix ftbfs in wallet-only mode

--- src/include/taler_crypto_lib.h
+++ src/include/taler_crypto_lib.h
@@ -22,7 +22,14 @@
 #ifndef TALER_CRYPTO_LIB_H
 #define TALER_CRYPTO_LIB_H
 
+#if HAVE_GNUNET_GNUNET_UTIL_LIB_H
 #include <gnunet/gnunet_util_lib.h>
+#endif
+#if HAVE_GNUNET_GNUNET_UTIL_TALER_WALLET_LIB_H
+#include <gnunet/gnunet_util_taler_wallet_lib.h>
+#endif
+#include "taler_util_wallet.h"
+
 #include <gcrypt.h>
 
 
--- src/util/amount.c
+++ src/util/amount.c
@@ -22,7 +22,12 @@
  * @author Christian Grothoff
  */
 #include "platform.h"
+#if HAVE_GNUNET_GNUNET_UTIL_LIB_H
 #include "taler_util.h"
+#endif
+#if HAVE_GNUNET_GNUNET_UTIL_TALER_WALLET_LIB_H
+#include "taler_util_wallet.h"
+#endif
 #include <gcrypt.h>
 
 
