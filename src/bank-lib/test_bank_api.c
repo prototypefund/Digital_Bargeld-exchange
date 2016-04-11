457638f07baaf32ea21f0552077dc2fafc1d6111
Florian Dold <florian.dold@gmail.com>
2016-04-11 23:58:02 +0200

    set currency to default bank currency for test case

--- src/bank-lib/test_bank_api.c
+++ src/bank-lib/test_bank_api.c
@@ -476,7 +476,7 @@ run (void *cls)
       .expected_response_code = MHD_HTTP_OK,
       .details.admin_add_incoming.credit_account_no = 1,
       .details.admin_add_incoming.debit_account_no = 2,
-      .details.admin_add_incoming.amount = "EUR:5.01" },
+      .details.admin_add_incoming.amount = "PUDOS:5.01" },
 
     { .oc = OC_END }
   };
