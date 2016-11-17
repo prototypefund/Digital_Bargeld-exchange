a90c12cbd91e4cc474141acdb3ecdc61c504e745
Christian Grothoff <christian@grothoff.org>
2016-11-17 14:47:47 +0100

    handle case rowid = 0

--- src/auditor/taler-auditor.c
+++ src/auditor/taler-auditor.c
@@ -112,9 +112,8 @@ handle_reserve_in (void *cls,
      matches wire transfers from the bank. Not sure if this should be
      done within the core auditor logic though... */
 
-  GNUNET_assert (rowid == reserve_in_serial_id + 1); /* should be monotonically increasing */
-  reserve_in_serial_id = GNUNET_MAX (rowid,
-                                     reserve_in_serial_id);
+  GNUNET_assert (rowid == reserve_in_serial_id); /* should be monotonically increasing */
+  reserve_in_serial_id = rowid + 1;
   GNUNET_CRYPTO_hash (reserve_pub,
                       sizeof (*reserve_pub),
                       &key);
@@ -174,9 +173,8 @@ handle_reserve_out (void *cls,
   struct ReserveSummary *rs;
 
   /* TODO: check signatures, in particluar the reserve_sig! */
-  GNUNET_assert (rowid == reserve_out_serial_id + 1); /* should be monotonically increasing */
-  reserve_in_serial_id = GNUNET_MAX (rowid,
-                                     reserve_out_serial_id);
+  GNUNET_assert (rowid == reserve_out_serial_id); /* should be monotonically increasing */
+  reserve_in_serial_id = rowid + 1;
   GNUNET_CRYPTO_hash (reserve_pub,
                       sizeof (*reserve_pub),
                       &key);
