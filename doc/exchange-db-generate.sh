baf60b619e2d4834c9ef800709afd83f26183e8d
Florian Dold <florian.dold@gmail.com>
2019-08-29 13:34:07 +0200

    move documentation to docs repo

--- doc/exchange-db-generate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-taler-exchange-dbinit -r
-java -jar schemaSpy_5.0.0.jar -t pgsql -db taler -o taler -host localhost -dp /usr/share/java/postgresql-jdbc3-9.2.jar -u grothoff -p test -s public -hq
