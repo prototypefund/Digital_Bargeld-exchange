76570207b033617bb7d591276941f1374632680c
Christian Grothoff <christian@grothoff.org>
2020-01-19 19:22:43 +0100

    fix copyright headers as per CA

--- src/exchangedb/test_exchangedb_denomkeys.c
+++ src/exchangedb/test_exchangedb_denomkeys.c
@@ -1,6 +1,6 @@
 /*
   This file is part of TALER
-  Copyright (C) 2014-2017 GNUnet e. V. (and other contributing authors)
+  Copyright (C) 2014-2017 Taler Systems SA (and other contributing authors)
 
   TALER is free software; you can redistribute it and/or modify it under the
   terms of the GNU General Public License as published by the Free Software
